4d393e9e3211d50aa1e9e2c32935ee228b99c2fc
王闻昊 <wwh27791@ly.com>
2016-12-01 22:05:05 +0800

    🚧使用pug视图引擎

--- app.js
+++ app.js
@@ -5,6 +5,10 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('view options', { layout: true });
+app.set('views', __dirname + '/views');
+
 app.get('/stooges/:name?', function (req, res, next) {
   var name = req.params.name;
 
@@ -12,7 +16,7 @@ app.get('/stooges/:name?', function (req, res, next) {
     case 'larry':
     case 'curly':
     case 'moe':
-      res.send(name + ' is my favorite stooge.');
+      res.render('stooges', { stooge: name });
       break;
 
     default:
@@ -21,11 +25,11 @@ app.get('/stooges/:name?', function (req, res, next) {
 });
 
 app.get('/stooges/*?', function (req, res) {
-  res.send('no stooges listed');
+  res.render('stooges', { stooge: null });
 });
 
 app.get('/?', function (req, res) {
-  res.send('hello world');
+  res.render('index');
 });
 
 var port = 8080;
